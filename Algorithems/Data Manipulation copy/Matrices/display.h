3d66340df63f5b6d420a346ac63b5843ae1a530e
Mehdislik <68567207+Mehdislik@users.noreply.github.com>
2020-10-31 20:45:51 +0100

    this files containt fonction related to display matrix and create it

---
+++ Algorithems/Data Manipulation copy/Matrices/display.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_H_INCLUDED
+#define DISPLAY_H_INCLUDED
+
+void fill_Matrix (int n, int **tab , int value);
+int ** create_Matrix(int n );
+void show_Matrix(int**t, int n);
+
+#endif // DISPLAY_H_INCLUDED
